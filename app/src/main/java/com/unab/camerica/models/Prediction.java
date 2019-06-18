cdede5073378eca63a7f4fa8f73228f10751af7f
Nicolas Ormeno <normeno@digevo.com>
2019-06-18 00:08:02 -0400

    Add Result as text

--- app/src/main/java/com/unab/camerica/models/Prediction.java
+++ app/src/main/java/com/unab/camerica/models/Prediction.java
@@ -5,21 +5,27 @@ import java.util.Map;
 public class Prediction {
 
     Long countryId1;
+    String countryName1;
     Long countryId2;
+    String countryName2;
     int res1;
     int res2;
 
     // Constructor
-    public Prediction(Long countryId1, Long countryId2, int res1, int res2) {
+    public Prediction(Long countryId1, String countryName1, Long countryId2, String countryName2, int res1, int res2) {
         this.countryId1 = countryId1;
+        this.countryName1 = countryName1;
         this.countryId2 = countryId2;
+        this.countryName2 = countryName2;
         this.res1 = res1;
         this.res2 = res2;
     }
 
     public Prediction(Map map) {
         this.countryId1 = (Long)map.get("countryId1");
+        this.countryName1 = (String)map.get("countryName1");
         this.countryId2 = (Long)map.get("countryId2");
+        this.countryName2 = (String)map.get("countryName2");
         this.res1 = Integer.parseInt(map.get("res1").toString());
         this.res2 = Integer.parseInt(map.get("res2").toString());
     }
@@ -32,12 +38,28 @@ public class Prediction {
         this.countryId1 = countryId1;
     }
 
+    // CountryName1
+    public String getCountryName1() {
+        return countryName1;
+    }
+    public void setCountryName1(String countryName1) {
+        this.countryName1 = countryName1;
+    }
+
     public Long getCountryId2() {
         return countryId2;
     }
 
     public void setCountryId2(Long countryId2) {
         this.countryId2 = countryId2;
+    }
+
+    // CountryName2
+    public String getCountryName2() {
+        return countryName2;
+    }
+    public void setCountryName2(String countryId1) {
+        this.countryName2 = countryName2;
     }
 
     public int getRes1() {
